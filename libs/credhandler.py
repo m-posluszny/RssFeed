6eae92c6718cb1228a06a4690df56864eea65fec
Mateusz Radomski <radomski.main@protonmail.com>
2020-12-04 13:13:24 +0100

    CredentialHandler: Basic skeleton of the class, waiting for database to be done.

---
+++ libs/credhandler.py
@@ -0,0 +1,33 @@
+from hashlib import sha1
+
+class CredentialsHandler:
+    def __init__(self, username, password):
+        self.__username = username
+        self.__password = password
+        self.__dbHandler = None
+
+    def doesUserExist(self):
+        result = None
+        # result = self.__dbHandler.findEntry(username)
+        if result == None:
+            return False
+        else:
+            return True
+        
+    def encryptCredentials(self):
+        hasher = sha1(self.__password)
+        self.__password = hasher.hexdigest()
+
+    def areCredValid(self):
+        # Password has to encrypted by this point
+        entry = None
+
+        #entry = self__dbHandler.findEntry(self.__username)
+
+        if entry == None:
+            return False
+        else:
+            if entry.getPassword() == self.__password:
+                return True
+            else:
+                return False
