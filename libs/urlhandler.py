458fe4c8afe03e13cd2419b66137b48156f465f0
Mateusz Radomski <radomski.main@protonmail.com>
2020-12-08 12:04:42 +0100

    URLHandler: Now able to tell is a string is a url.

--- libs/urlhandler.py
+++ libs/urlhandler.py
@@ -1,3 +1,5 @@
+import re
+
 class URLHandler:
     def addURL(url):
         pass
@@ -13,3 +15,16 @@ class URLHandler:
 
     def getMostPopularURLs():
         pass
+
+    def stringIsURL(self, url):
+        regex = re.compile(
+                r'^(?:http|ftp)s?://' # http:// or https://
+                r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|' #domain...
+                r'localhost|' #localhost...
+                r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})' # ...or ip
+                r'(?::\d+)?' # optional port
+                r'(?:/?|[/?]\S+)$', re.IGNORECASE)
+
+        res = re.match(regex, url) is not None
+
+        return res
