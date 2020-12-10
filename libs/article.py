a1ad251a646dfe8c8d56dbd078559a6ed03cf3d0
Mateusz Radomski <radomski.main@protonmail.com>
2020-12-10 17:27:02 +0100

    RSS: Small refactor.

--- libs/article.py
+++ libs/article.py
@@ -1,8 +1,20 @@
+from libs.rss import RSSHeader
+
 class Article:
-    def __init__(self, title, link, content, date):
-        self.__title = title
-        self.__link = link
-        self.__content = content
+    def __init__(self, title, link, desc, date):
+        self.__header = RSSHeader(title, link, desc)
         self.__date = date
     def __str__(self):
-        return "[{}][{}][{}][{}]".format(self.__title, self.__date, self.__link, self.__content)
+        return "[{}][{}][{}][{}]".format(self.title, self.__date, self.link, self.content)
+
+    @property
+    def title(self):
+        return self.__header.title
+
+    @property
+    def link(self):
+        return self.__header.link
+
+    @property
+    def content(self):
+        return self.__header.desc
