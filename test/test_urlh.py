60921d25700cfd5182024c9619b160b572c66f7e
Mateusz Radomski <radomski.main@protonmail.com>
2020-12-12 13:44:52 +0100

    Test: Basic tests of URLHandler that all seem to be passing.

--- test/test_urlh.py
+++ test/test_urlh.py
@@ -1,5 +1,6 @@
 import unittest
 from libs.urlhandler import URLHandler
+from libs.databasehandler import DatabaseHandler
 
 class TestURLHandler(unittest.TestCase):
 
@@ -28,3 +29,66 @@ class TestURLHandler(unittest.TestCase):
         self.assertEqual(urlh.stringIsURL(':feeds.megaphone.fm/unlocking-us'), False)
         self.assertEqual(urlh.stringIsURL('ht/feeds.megaphone.fm/ADL9840290619'), False)
         self.assertEqual(urlh.stringIsURL('https:\\feedsnpro/////rg510313podcas'), False)
+
+    def test_basic_operations(self):
+        from libs.credhandler import CredentialsHandler
+
+        db = DatabaseHandler(db_path = "pytests/", dbIsTemp = True)
+        cdh = CredentialsHandler('unittest', 'unittestpassword')
+        cdh.encryptCredentials()
+
+        self.assertEqual(cdh.doesUserExist(), False)
+        cdh.createUser()
+        self.assertEqual(cdh.doesUserExist(), True)
+
+        url = 'https://feeds.simplecast.com/6HzeyO6b'
+        urlh = URLHandler()
+
+        res = db.getEntry('unittest')
+        self.assertNotEqual(len(res['urls']), 0)
+        self.assertEqual(len([l for l in res['urls'] if l['actual_url'] == url]), 0)
+
+        URLHandler.addURL(url)
+        
+        res = db.getEntry('unittest')
+        self.assertNotEqual(len(res['urls']), 0)
+        self.assertEqual(len([l for l in res['urls'] if l['actual_url'] == url]), 1)
+        
+        DatabaseHandler.destroyDatabase()
+
+    def test_basic_operations_with_fetching(self):
+        from libs.credhandler import CredentialsHandler
+        from libs.rsshandler import RSSHandler
+
+        db = DatabaseHandler(db_path = "pytests/", dbIsTemp = True)
+        cdh = CredentialsHandler('unittest', 'unittestpassword')
+        cdh.encryptCredentials()
+
+        cdh.createUser()
+
+        url = 'https://feeds.simplecast.com/6HzeyO6b'
+        URLHandler.addURL(url)
+        
+        res = db.getEntry('unittest')
+        self.assertNotEqual(len(res['urls']), 0)
+        self.assertEqual(len([l for l in res['urls'] if l['actual_url'] == url]), 1)
+
+        urlentry = next(filter(lambda x: x['actual_url'] == url, res['urls']))
+        self.assertEqual(len(urlentry['articles']), 0)
+
+        rssh = RSSHandler()
+        rssh.retriveDataFromURL(url)
+        self.assertEqual(rssh.fetchIsSuccess(), True)
+
+        rssh.parseXML()
+        art = rssh.returnArticles()
+
+        self.assertNotEqual(len(art), 0)
+
+        URLHandler.appendDownloadedArticles(url, art)
+
+        res = db.getEntry('unittest')
+        urlentry = next(filter(lambda x: x['actual_url'] == url, res['urls']))
+        self.assertNotEqual(len(urlentry['articles']), 0)
+
+        DatabaseHandler.destroyDatabase()
