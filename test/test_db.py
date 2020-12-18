e89d819def71d5b5df19df746393c1b9a70ae429
Mateusz Radomski <radomski.main@protonmail.com>
2020-12-18 14:28:58 +0100

    DatabaseHandler: Switched to LMDB cuz Mozzila says it's faster.

--- test/test_db.py
+++ test/test_db.py
@@ -6,17 +6,13 @@ class TestURLHandler(unittest.TestCase):
     def test_basic_operations(self):
         db = DatabaseHandler(db_path = "pytests/", dbIsTemp = True)
 
-        self.assertEqual(db.databaseOnline, True)
-
         db.addEntry('testkey', '\"testvalue\"')
-
-        self.assertEqual(db.databaseOnline, True)
 
         res = db.getEntry('testkey')
         self.assertEqual(res, "testvalue")
 
         res = db.deleteEntry('testkey')
-        self.assertEqual(res, None)
+        self.assertEqual(res, True)
 
         res = db.getEntry('nonvalidtestkey')
         self.assertEqual(res, None)
@@ -27,14 +23,10 @@ class TestURLHandler(unittest.TestCase):
         import json
         db = DatabaseHandler(db_path = "pytests/", dbIsTemp = True)
 
-        self.assertEqual(db.databaseOnline, True)
-
         l = [str(i) for i in range(100)]
         asStr = json.dumps(l)
 
         db.addEntry('testkey', asStr)
-
-        self.assertEqual(db.databaseOnline, True)
 
         res = db.getEntry('testkey')
         self.assertNotEqual(res, None)
